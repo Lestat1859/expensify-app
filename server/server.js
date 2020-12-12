f436656f43bb7c65d498251adbec1aef54d5f3f2
Maxime Delannoy <maxime.delannoy.dev@gmail.com>
2020-12-12 16:14:50 +0100

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require ('path');
 const express = require('express');
 const app = express(); // creation de l'application express
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath)); // on dit a express d'utiliser le dossier public pour charger les pages statiques
 
@@ -9,6 +10,6 @@ app.get('*',(req, res)=>{
     res.sendFile(path.join(publicPath, 'index.html')); // on rerouter les requetes differentes du publicFolder vers index.html
 });
 
-app.listen(3000,() => { // démarre le server sur le port 3000
+app.listen(port,() => { // démarre le server sur le port 3000
     console.log('Server is up!')
 });
