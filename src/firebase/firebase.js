90287780c829c83f4711d4a9daf62cc88da2b9ec
Maxime Delannoy <maxime.delannoy.dev@gmail.com>
2020-12-22 13:20:30 +0100

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase'; // "* as" récupère toutes les fonctions de la bibliothèqu firebase pour qu'on puisse les utiliser dans l'objet firebase
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCirBZClgJH4rscFr1NR5x1ffn9rT_XYgs",
-  authDomain: "expensify-cafd0.firebaseapp.com",
-  databaseURL: "https://expensify-cafd0-default-rtdb.firebaseio.com",
-  projectId: "expensify-cafd0",
-  storageBucket: "expensify-cafd0.appspot.com",
-  messagingSenderId: "443360513111",
-  appId: "1:443360513111:web:acbe3e80ebc74cfdcae712"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.default.initializeApp(firebaseConfig);
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -14,6 +14,13 @@ console.log('before');
 // la fonction then est al callback qui sera lancé une fois la promesse réussie
 promise.then((data)=>{
   console.log('1',data);
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('This is my other promise');  
+    },5000)
+  });
+}).then((str)=>{
+  console.log('does this run', str);
 }).catch((error)=>{
   console.log('error: ',error)
 });
